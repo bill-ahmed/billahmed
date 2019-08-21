fab10de6485f33c76bf15da1b0377d2a340cbcfa
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-08-21 19:25:55 -0400

    js animations for gallery in hobbies section

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -43,12 +43,71 @@ const useStyles = makeStyles(theme => ({
     },
   }));
 
+/**Animate an image into a parent element
+ * @param imageID The unique id of the DOM element
+ * @param imageClicked Value of the image that was clicked, given by "require(path/to/file/jpg)" 
+ */
+function animateImageClick(imageID, imageClicked){
+    // The image that was clicked in the gallery
+    let imgClicked = document.getElementById(imageID);
+    let imgClickedOrigin = imgClicked.getBoundingClientRect()
+
+    // The image that will be displayed
+    let imgToDisplay = document.createElement("img");
+
+    // Calculate it's original dimensions
+    let originalWidth = imgClickedOrigin.right - imgClickedOrigin.x;
+    let originalHeight = imgClickedOrigin.bottom - imgClickedOrigin.y;
+
+    // Parent of where this image is going to be placed
+    let imgParent = document.getElementById("imageViewerContainer");
+    let imgParentOrigin = imgParent.getBoundingClientRect();
+
+    imgToDisplay.setAttribute("src", imageClicked);
+    imgToDisplay.setAttribute("width", "99%");
+    imgToDisplay.setAttribute("height", "99%");
+
+    // [from {}, to {}]
+    let fadeInAnimation = [{
+        opacity: 0.5,
+    }, {
+        opacity: 1,
+    }]
+
+    let fadeInAndGrow = [{
+        opacity: 0.5,
+        width: "95%",
+        height: "95%"
+    }, {
+        opacity: 1,
+        width: "99%",
+        height: "99%"
+    }]
+
+
+    // If current child already exists
+    if(imgParent.children.length !== 0){
+        // Remove it
+        imgParent.removeChild(imgParent.children[0]);
+
+        // Animate the element; (keyframes, duration_of_animation)
+        imgToDisplay.animate(fadeInAnimation, {easing: 'ease-in', duration: 400});
+    } else{
+        // Animate fade in and grow
+        imgToDisplay.animate(fadeInAndGrow, {easing: 'ease-in', duration: 800});
+    }
+
+    imgParent.appendChild(imgToDisplay)
+
+    console.log(imgClicked.getBoundingClientRect());
+
+}
 
 /**A component to represent the hobbies section */
 function Hobbies(props) {
 
     // Keep track of which image is selected, so appropriate one can be displayed larger
-    const [currImage, setCurrImage] = useState(require('../assets/image_placeholder.jpg'));
+    const [currImage, setCurrImage] = useState();
 
     var classes = useStyles();  // Styles added to GridList and other elements
     var isMobileUser = props.isMobileUser();
@@ -61,8 +120,9 @@ function Hobbies(props) {
     /**Hadle user clicking on an image
      * @param imageClicked The image the use has clicked, given by require('path/to/file.jpg')
      */
-    const handleImageClick = (imageClicked) => {
-        setCurrImage(imageClicked);
+    const handleImageClick = (imgID, imageClicked) => {
+        animateImageClick(imgID, imageClicked);
+        //setCurrImage(imageClicked);
     }
 
     return(
@@ -77,9 +137,10 @@ function Hobbies(props) {
                                 although I do ocassionally attempt to photograph moving objects. The following is a small sample
                                 of the images i've taken.
                             </p>
-                            <div className="imageViewerContainer">
-                                <img width="99%" height="99%" src={currImage} alt="test"/>
+                            <div className="imageViewerContainer" id="imageViewerContainer">
+                                {currImage && <img width="99%" height="99%" src={currImage} alt=""/>}
                             </div>
+                            <p><strong>Click an image to enlarge it here.</strong></p>
                         </div>
                     </div>
 
@@ -87,8 +148,8 @@ function Hobbies(props) {
                         <GridList cellHeight={270} cols={6} className={classes.gridList}>
                             {portfolioData.map(tile => (
 
-                                <GridListTile onClick={() => handleImageClick(tile.img)} key={tile.img} cols={tile.cols || 1}>
-                                    <img src={tile.img} alt={tile.title}/>
+                                <GridListTile onClick={() => handleImageClick(tile.title, tile.img)} key={tile.img} cols={tile.cols || 1}>
+                                    <img src={tile.img} alt={tile.title} id={tile.title}/>
 
                                     <GridListTileBar title={tile.title}/>
                                 </GridListTile>
