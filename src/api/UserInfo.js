a223426dfc6edd4b1b5d97eb34c962d7446cb815
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-04-17 19:35:08 -0400

    nuke react version

--- src/api/UserInfo.js
+++
@@ -1,9 +0,0 @@
-/**Determine if current user is a mobile user or not
-* @returns true iff mobile user, false otherwise
-*/
-export default function isMobileUser(){
- if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent)) {
-   return true;
- }
- return false;
-}
