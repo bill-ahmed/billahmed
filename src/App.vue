1dd01a0666cd3bbfa65781995387d636a9f0570b
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-04-17 21:43:07 -0400

    add buetify, some content for main page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
-    <div class="spacer-5"/>
-    <div class="nrow space-between">
-      <div class="ncol" style="margin-left: 70px;">
+    <div class="nrow grow space-between">
+      <div v-if="isSelected('Home')" class="ncol" id="tab-1-content" style="margin-left: 70px;">
         <div class="nrow">
           <div id="name">
             Bilal Ahmed
@@ -17,14 +16,29 @@
           </p>
         </div>
 
-        <div class="nrow space-evenly">
+        <div class="nrow" style="margin-top: 50px">
+          <b-button type="is-dark" size="is-large" rounded icon-left="github" @click="open('https://github.com/bill-ahmed')">
+            
+          </b-button>
+
+          <b-button type="is-dark" size="is-large" rounded icon-left="linkedin" @click="open('https://www.linkedin.com/in/bill-ahmed')">
+          </b-button>
+
+          <b-button type="is-dark" size="is-large" rounded icon-left="download" @click="open('/')">
+            My Resume
+          </b-button>
         </div>
       </div>
 
-      <div class="ncol" style="margin-right: 70px;">
-        
+      <div id="right-nav" class="ncol justify-center">
+        <b-button v-for="tab in tabs" :key="tab" 
+                  type="is-dark"
+                  @click="handleTabClick(tab)" 
+                  :outlined="!isSelected(tab)" 
+                  class="b-button" :size="isSelected(tab) ? 'is-large' : 'is-medium'">
+         {{ tab }}
+        </b-button>
       </div>
-
     </div>
   </div>
 </template>
@@ -37,14 +51,57 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class App extends Vue {
+  tabs = [
+    'Home',
+    'Projects',
+    'Education'
+  ]
 
+  selected = this.tabs[0];
+
+  handleTabClick(tab: string) {
+    this.selected = tab;
+  }
+
+  open(url: string) {
+    window.open(url, '_blank')
+  }
+
+  isSelected(tab: string) {
+    return this.selected === tab;
+  }
 }
 </script>
 
-<style scoped>
+<style>
+.b-button {
+  margin: 15px;
+}
+
+.button {
+  margin-right: 15px;
+}
+
+.step-item {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+#right-nav {
+  margin-right: 40px;
+}
+
+#tab-1-content {
+  height: 100%;
+  justify-content: center;
+
+  min-width: 600px;
+}
+
 #app {
   width: 100%;
-  height: 100%;
+  height: 100vh;
 
   display: flex;
   flex-direction: column;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//@ts-ignore
+import { Button, Steps } from 'buefy'
+
+import 'buefy/dist/buefy.css'
 import './main.css'
 
+Vue.use(Button)
+Vue.use(Steps)
 Vue.config.productionTip = false
 
 new Vue({
