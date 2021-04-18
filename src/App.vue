b393bcafa2b360a65b499e37961ba3a5cc93a92b
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-04-17 20:11:03 -0400

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,62 @@
 <template>
   <div id="app">
-    Empty...
+    <div class="spacer-5"/>
+    <div class="nrow space-between">
+      <div class="ncol" style="margin-left: 70px;">
+        <div class="nrow">
+          <div id="name">
+            Bilal Ahmed
+          </div>
+        </div>
+
+        <div class="nrow" id="short_info">
+          <p>
+            A <b>Computer Science</b> student at the <br/>
+          University of Toronto that's passionate about <br/> 
+          <b>web design</b> and <b>software development</b>.
+          </p>
+        </div>
+
+        <div class="nrow space-evenly">
+        </div>
+      </div>
+
+      <div class="ncol" style="margin-right: 70px;">
+        
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+}
 </script>
 
-<style>
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
 
+  display: flex;
+  flex-direction: column;
+}
+
+#name {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 7rem;
+}
+
+#short_info {
+  font-size: 1.8rem;
+  font-weight: 100;
+}
 </style>
